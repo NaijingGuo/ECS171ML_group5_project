336fe87c906fa7382bef3f499b96fb64d533184e
Eva Chen <evachen@ucdavis.edu>
2021-11-24 13:54:07 -0800

    frontend page display

--- App/app.py
+++ App/app.py
@@ -1,6 +1,13 @@
 import numpy as np
 from flask import Flask, request, render_template
 from keras.models import load_model 
+
+# import easyforms
+
+# @app.route('/some-url', method=['GET', 'POST'])
+# def some_view_function():
+    
+
 # Create flask app
 flask_app = Flask(__name__)
 model = load_model('AHPTtrainedNNmodel.h5')
@@ -24,9 +31,14 @@ mapping_labels = {
 def Home():
     return render_template("index.html")
 
+
 @flask_app.route("/predict", methods = ["POST"])
 def predict():
     features = []
+    print('Entered Predict')
+    for value in request.form.values():
+        print(value)
+    # print(request.form.values())
     for value in request.form.values():
         if value in mapping_features:
             features.append(mapping_features[value])
@@ -34,7 +46,70 @@ def predict():
             features.append(value)
     features = np.array([features], dtype=np.float)
     prediction = str(np.argmax(model.predict(features)))
-    return render_template("index.html", prediction_text = "The health classification is {}".format(mapping_labels[prediction]))
+
+    out = {}
+
+    if request.form['Gender'] == 'No':
+        out['Gender?'] = "Female" 
+    else:
+        out['Gender?']="Male"
+    out['History of Obesity in Family?'] = request.form['FHO']
+    out['Frequently Consume High Caloric Food?'] = request.form['FAVC']
+    out['Frequently Consume Vegetables?'] = request.form['FCVC']
+    if request.form['CAEC'] == 'nalc':
+        out['Eat Between Meals Often?'] = "Never"
+    elif request.form['CAEC'] == 'salc':
+        out['Eat Between Meals Often?'] = "Sometimes"
+    elif request.form['CAEC'] == 'falc':
+        out['Eat Between Meals Often?'] = "Freqently"
+    else:
+        out['Eat Between Meals Often?'] = "Always"
+    out['Smoke?'] = request.form['SMOKE']
+    if request.form['CH20'] == 'lncfm':
+        out['Water Consumption?'] = "Less than a liter"
+    elif request.form['CAEC'] == 'bncfm':
+        out['Water Consumption?'] = "Between 1 and 2 L"
+    else:
+        out['Water Consumption?'] = "More than 2 L"
+
+    out['Monitor Daily Calories?'] = request.form['SCC']
+    
+    if request.form['FAF'] == 'npa':
+        out['Freqency of Physical Activity?'] = "Never"
+    elif request.form['CAEC'] == 'spa':
+        out['Freqency of Physical Activity?'] = "1 or 2 days"
+    elif request.form['CAEC'] == 'gpa':
+        out['Freqency of Physical Activity?'] = "2 or 4 days"
+    else:
+        out['Freqency of Physical Activity?'] = "4 or 5 days"
+    
+    if request.form['TUE'] == 'nds':
+        out['Time Spent on Devices Per Day?'] = "0-2 hours"
+    elif request.form['CAEC'] == 'sds':
+        out['Time Spent on Devices Per Day?'] = "3-5 hours"
+    elif request.form['CAEC'] == 'gds':
+        out['Time Spent on Devices Per Day?'] = "More than 5 hours"
+    else:
+        out['Time Spent on Devices Per Day?'] = "Always"
+
+    if request.form['CALC'] == 'nalc':
+        out['Alcohol Consumption?'] = "Never"
+    elif request.form['CAEC'] == 'salc':
+        out['Alcohol Consumption?'] = "Sometimes"
+    elif request.form['CAEC'] == 'falc':
+        out['Alcohol Consumption?'] = "Freqently"
+    else:
+        out['Alcohol Consumption?'] = "Always"
+
+    if request.form['mtrans'] == 'pub':
+        out['Primary Mode of Transportation?'] = "Public Transportation"
+    else:
+        out['Primary Mode of Transportation?'] = request.form['mtrans']
+
+    out['Age'] = request.form['Age']
+    out['Meal Per Day'] = request.form['NCP']
+
+    return render_template("out.html", result = out,prediction_text = "The health classification is {}".format(mapping_labels[prediction]))
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
